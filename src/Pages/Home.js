9631ac305b6456e4e914a176cc505250885d7870
abhiamber191 <abhiamber191@gmail.com>
2023-01-22 23:30:25 +0530

    downloadbtnupdated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,42 +5,49 @@ import style from "../Style/Home.module.css";
 import About from "./About";
 import Contact from "./Contact";
 import Project from "./Project";
-import Skills from "./Skills"
+import Skills from "./Skills";
 // import Abhishek_Amber_Resume from "../resume/Abhishek_ Amber_Resume.pdf"
 
-import { TypeAnimation } from 'react-type-animation';
+import { TypeAnimation } from "react-type-animation";
 
 function Home() {
+  const handleClick = () => {
+    window.open(
+      "https://drive.google.com/file/d/1M3hLDMToWV3vpjh5AjfpCT48AN1DyFgE/view?usp=sharing",
+      "_blank"
+    );
+  };
   return (
     <div className="mainBox">
       <div className={style.container}>
         <div className={style.title}>
           <p> Hello, I'm a</p>
 
-          <h2> 
-          
-          <TypeAnimation
-    sequence={['Full Stack Web Developer', 100, '']}
-    speed={35} 
-    wrapper="h3"
-    
-    repeat={Infinity}
-  /></h2>
+          <h2>
+            <TypeAnimation
+              sequence={["Full Stack Web Developer", 100, ""]}
+              speed={35}
+              wrapper="h3"
+              repeat={Infinity}
+            />
+          </h2>
 
-          <a href="https://drive.google.com/uc?id=1M3hLDMToWV3vpjh5AjfpCT48AN1DyFgE&export=download">
-            <button>Resume</button>
+          <a
+            href="https://drive.google.com/uc?id=1M3hLDMToWV3vpjh5AjfpCT48AN1DyFgE&export=download"
+            // target="_blank"
+          >
+            <button onClick={handleClick}>Resume</button>
           </a>
         </div>
       </div>
 
       <div className="abooutSection">
-      
-        <About  className={style.about}/>
-        <Skills className={style.Skills}/>
-        <Project className={style.Project}/>
-        <GIthub/>
-        <Contact className={style.Contact}/>
-        <Footer className={style.Footer}/>
+        <About className={style.about} />
+        <Skills className={style.Skills} />
+        <Project className={style.Project} />
+        <GIthub />
+        <Contact className={style.Contact} />
+        <Footer className={style.Footer} />
       </div>
     </div>
   );
@@ -48,6 +55,5 @@ function Home() {
 export default Home;
 
 // https://drive.google.com/file/d/1M3hLDMToWV3vpjh5AjfpCT48AN1DyFgE/view?usp=sharing
-
 
 // https://drive.google.com/uc?id=1M3hLDMToWV3vpjh5AjfpCT48AN1DyFgE&export=download
